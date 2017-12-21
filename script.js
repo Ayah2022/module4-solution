e7bfc2ef90e253238ef44cda9c259cd07eb5f318
Ayah2022 <aya.rateb2022@yahoo.com>
2017-12-22 00:43:39 +0200

    finish assignment

--- script.js
+++ script.js
@@ -41,7 +41,9 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for (name in names) {
+	var firstLetter = names[name].charAt(0);
+	
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -57,9 +59,9 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (firstLetter.toLowerCase() == 'j') {
+    byeSpeaker.speak(names[name]);
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[name]);
   }
 }
